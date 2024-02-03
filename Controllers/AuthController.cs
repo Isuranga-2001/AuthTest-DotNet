5df5a9f32d424eb43166a74b066fbd8a672fc842
Isuranga Warnasooriya <59173608+Isuranga-2001@users.noreply.github.com>
2024-02-04 00:58:06 +0530

    Add Project Files

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserAuthTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+    }
+}
